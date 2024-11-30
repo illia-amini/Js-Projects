4dbb7825ff7338f3cd87b8367ce31ae6b61136f5
illia-amini <illiaamini2002ir@gmail.com>
2024-11-30 03:33:10 +0330

    First-Practice-of-javascr
    Variables
    Syntax
    Operations

---
+++ Assect/custome.js
@@ -0,0 +1,13 @@
+//<========Biography========>
+var $name='ali';
+var $name='amini'
+var $age=22;
+var $height=188;
+var $weight=81;
+//<========Biography========>
+//<========ex-01========>
+let x=prompt('Enter ur length;');
+let y=prompt('Enter ur width;');
+let z=(x*y);
+console.log('Ur Area is',z);
+//<========ex-01========> 
