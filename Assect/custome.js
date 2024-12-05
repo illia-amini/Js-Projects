bf618b4dc26c2480800009c0c0fcdaa675741a59
illia-amini <illiaamini2002ir@gmail.com>
2024-12-06 02:56:49 +0330

    Hm-01,Started
     start on;
    6/dec/2024

---
+++ Assect/custome.js
@@ -0,0 +1,41 @@
+//Hm-01
+
+//01-s,multidimensional-Array
+
+var Array01=[1,2,[45,56,43],3,4];
+Array01[2][3]='add a random num here';
+console.log(Array01);
+
+//01-e,multidimensional-Array
+
+//02-s-Datatype
+//1-num
+var Age = 22;//int
+var Mathexam = 90.2;//float
+var Cputemp = 26;//int
+var Ceratine = 12.6;//float
+var Fingers = 10;//int
+var Bananaprice = 75.4;//float
+var Days = 7;//int
+var Airpollution = 100.2;//float
+var Continents = 7;//int
+var Petrollitre; 25.6//float
+//2-str
+var Name = 'Ali';//str
+var Surename = 'Amini';//str
+var Fathername= 'Reza';//str
+var email = 'illiaamini2002ir@gmail.come';//str
+var Pass ='12345';//str
+var Dateofbirth = '26/06/2002';//str
+var Sex = 'Male';//str
+var Single = 'True';//str
+var Major = 'Electrical&Electronic engineering';//str
+var Uni = 'Lyon Univercity';//str
+//3-bool
+
+//4-array;
+
+//5-object;
+
+//02-e-Datatype
+//
