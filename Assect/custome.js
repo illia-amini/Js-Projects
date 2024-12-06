d8578496e0ac4d19b52027561d757c93103efd32
illia-amini <illiaamini2002ir@gmail.com>
2024-12-06 23:36:29 +0330

    Update custome.js

--- Assect/custome.js
+++ Assect/custome.js
@@ -5,9 +5,9 @@
 /*
 .1-ex; write a code that insert nums to the end of index2. use push() & normal way.
      array=[1,2,[45,56,43,],3,4]
-.2- 10 ex for each datatype.
-.3- console.table(), write with students ex.
-.4- read and write a code for console 5 or 6 at last. 
+.2-10 ex for each datatype.
+.3-console.table(), write with students ex.
+.4-study and write a code for console 5 or 6 at last. 
 .5-javascript reffrence; w3schools, math section 10 code.
 .6-javascript reffrence; w3schools, array section 5 code. 
 .7-javascript reffrence; w3schools, string array section 3/5 code. 
@@ -25,6 +25,7 @@ console.log(Array01);
 //01-e,multidimensional-Array
 
 //02-s-Datatype
+
 //1-num
 var Age = 22;//int
 var Mathexam = 90.2;//float
@@ -48,54 +49,179 @@ var Single = 'True';//str
 var Major = 'Electrical&Electronic engineering';//str
 var Uni = 'Lyon Univercity';//str
 //3-bool
-var Isodd = true ;//bool
-var Iseven = false ;//bool
-var Istrue = true //bool
-var Isfalse = false ;//bool
-var Is=  ;//bool
-var Is=  ;//bool
-var Is=  ;//bool
-var Is=  ;//bool
-var Is=  ;//bool
-var Is=  ;//bool
+var Isodd = true ;//bool#1
+var Iseven = false ;//bool#0
+var Istrue = true //bool#1
+var Isfalse = false ;//bool#0
+var Ismarried = true ;//bool#1
+var Isday= false ;//bool#0
+var Isnight= true ;//bool#1
+var Isexpensive= false;//bool#0
+var Ischeap= true;//bool#1
+var Isbig= false ;//bool#0
 //4-array;
-var = ;//array
-var = ;//array
-var = ;//array
-var = ;//array
-var = ;//array
-var = ;//array
-var = ;//array
-var = ;//array
-var = ;//array
-var = ;//array
+//#use-[],brackets.
+var Array01 =[1,3,5,7,9] ;//array#odd-num
+var Array02 = [2,4,6,8,];//array#even-num
+var Array03 =[2,3,5,7] ;//array#prime-num
+var Array04 =['ali','reza','amini'] ;//array#str
+var Array05 =['alireza','amini','22'] ;//array#str
+var Array06 =['html','css','js'] ;//array#str
+var Array07 =['bool','str','num','object','array'] ;//array#str
+var Array08 =['true','false','0','1','ali'] ;//array#str
+var Array09 =[1,2,3[4,5,6],7,8,[9,10]] ;//array#multidiemeinsion-2d
+var Array10 =[1,2,3,[4,5,6,[7,8,9]],10] ;//array#multidiemeinsion-3d
 //5-object;
-var = ;//object
-var = ;//object
-var = ;//object
-var = ;//object
-var = ;//object
-var = ;//object
-var = ;//object
-var = ;//object
-var = ;//object
-var = ;//object
+//#use-{},curely brackets.
+var Object01 = {Age:22,Firstname:'Illia',Lastname:'Amini' };//object#bio
+var Object02 = {Birth:2002,Gender:'Male'};//object#bio-02
+var Object03 = {Favsport:'Archery',Favsport02:'Socer',Trophy:3};//object#bio-3
+var Object04 = {Edu:'Enginear',Uni:'Lyon uni',Books:0};//object#bio-04
+var Object05 = {Weigth:80.5,Height:188.8,Skin:'white'};//object#bio-05
+var Object06 = {Favmovie:'Nobody',Favseries:'Thlo',Timewatched:3};//object#bio-06
+var Object07 = {Favfood:'Nypizza',Favfood02:'Kebab',Eatinweek:2};//object#bio-07
+var Object08 = {Married:false,Programmer:true,Student:true};//object
+var Object09 = [
+    std1=
+    {
+        Name:'illia',
+        Surename:'amini',
+        Age:22,
+        Gender:'male',
+        Major:'EEE',
+        Athelete:true,
+        Marriead:false,
+        Child:0
+    }
+    ,
+    std2=
+    {
+        Name:'amir',
+        Surename:'mortazavi',
+        Age:29,
+        Gender:'male',
+        Major:'medical',
+        Athelete:false,
+        Marriead:true,
+        Child:0
+    }
+    ,
+    std3=
+    {
+        Name:'elena',
+        Surename:'amiri',
+        Age:56,
+        Gender:'female',
+        Major:'retierd',
+        Athelete:false,
+        Marriead:true,
+        Child:3
+    }
+];//object#object & array-01
+var Object10 = [
+        Mybio=
+    {
+        Name:'illia',
+        Surename:'Amini',
+        Age:22,
+        Gender:'Male',
+        Birth:2002,
+        Occupation:'Stuudent',
+        Major:'EEE',
+        Athlete:true,
+        Sport:'Archery',
+        Married:false,
+        child:0,
+        Favfood:'Pizza',
+    }
+];//object#object & array-02
+/*
+:)
+to-run-any-of-wrriten-codes-above\
+only-write-either-consol.log();-or-consol.table();\
+visit-the-result-on-consile-section\
+:)
+*/
+
 //02-e-Datatype
 
-//03-s-
+//03-s-consol.table()
 
-//04-s-
+var Students = [
+    std1=
+    {
+        Name:'illia',
+        Surename:'amini',
+        Age:22,
+        Gender:'male',
+        Major:'EEE',
+        Athelete:true,
+        Marriead:false,
+        Child:0
+    }
+    ,
+    std2=
+    {
+        Name:'amir',
+        Surename:'mortazavi',
+        Age:29,
+        Gender:'male',
+        Major:'medical',
+        Athelete:false,
+        Marriead:true,
+        Child:0
+    }
+    ,
+    std3=
+    {
+        Name:'elena',
+        Surename:'amiri',
+        Age:56,
+        Gender:'female',
+        Major:'retierd',
+        Athelete:false,
+        Marriead:true,
+        Child:3
+    }
+]
+console.table(Students);
 
-//05-s-
+//03-e-consol.table()
 
-//06-s-
+//04-s-consol
 
-//07-s-
+//04-e-consol
+
+//05-s-math
+
+//05-e-math
+
+//06-s-array
+
+//06-e-array
+
+//07-s-string
+
+//07-e-string
 
 //08-s-
+/*
+All-of-the-projects-were-published-on-my-githubpanel-visit-via-links-below.
+#illia-amini
+#https://github.com/illia-amini/javascr-test.git
+*/
+//08-e-
 
 //09-s-
+/*
+untill- -videos-is-watched-by-illiaamini.
+*/
+//09-e-
 
 //10-s-
+/*
+the-given-book-is-read-by-illiaamini.
+*/
+//10-e-
 
 //
