a29c7fd730710929169b361a0612936d83f41d67
illia-amini <illiaamini2002ir@gmail.com>
2024-12-18 18:38:59 +0330

    Update Password-pr.js

--- Assect/Password-pr.js
+++ Assect/Password-pr.js
@@ -1,4 +1,16 @@
 //A-simple-Password-code-with-js
-
-//written-by-illia.amini
+var $userName = prompt('Enter ur Username;');//recive user's usname.
+var $passWord = prompt('Enter ur Password;');//recive user's pass.
+if($userName =='illia_amini' && $userPass== '2002.ir'){
+    alert(`Welcome to your account,\n${userName}`);//welcome notf.
+    alert('For ore saftey plz choose one of the operation below,');//user choose op.
+    alert(`1-Math\n2-Geomatri\n3-Leapyear\n4-Max or Min\n5-Quadratic Eq`);//user options.
+    var $userFirstChoose = parseInt(prompt('Number:'));//Number given.
+    switch($userFirstChoose){
+        case 1:{}case 2:{}case 3:{}case 4:{}case 5:{}
+    }
+}else{
+    alert('Wrong Password or Username')
+}
+//w{ritten-by-illia.amini
 //mind-of-king-,-hear-of-warrior
