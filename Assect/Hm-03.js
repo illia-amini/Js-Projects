ec996d970f5befe8f7e8f7ea417542199f48ab73
illia-amini <illiaamini2002ir@gmail.com>
2024-12-19 17:55:49 +0330

    Hm03-added

---
+++ Assect/Hm-03.js
@@ -0,0 +1,12 @@
+//Hm-03;
+
+//instrouction-below:
+//camelCase-written;
+/*
+.1-solve,untill 23 ex of programiz js section.
+.3-read textbooks,until page 
+.4-code with mosh loop & function videos
+*/
+
+//written-by-illia.amini
+//mind-of-king-,-hear-of-warrior
