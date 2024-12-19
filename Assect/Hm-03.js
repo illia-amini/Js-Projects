42bcdf20b8e9c51520c250595559477cfd56eacd
illia-amini <illiaamini2002ir@gmail.com>
2024-12-19 18:01:21 +0330

    Update Hm-03.js

--- Assect/Hm-03.js
+++ Assect/Hm-03.js
@@ -4,8 +4,10 @@
 //camelCase-written;
 /*
 .1-solve,untill 23 ex of programiz js section.
-.3-read textbooks,until page 
-.4-code with mosh loop & function videos
+.3-read textbooks,until page.
+.4-code with mosh loop & function videos.
+.5-For in & for of loop,self study on w3shools.
+.6-Rewrite,first season of web3 course in for moduel.
 */
 
 //written-by-illia.amini
