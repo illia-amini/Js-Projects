e56f5be6e5a2cc9099e32ffb723af709fb3bd5fe
illia-amini <illiaamini2002ir@gmail.com>
2024-12-21 23:28:48 +0330

    Update Hm-03.js

--- Assect/Hm-03.js
+++ Assect/Hm-03.js
@@ -4,11 +4,60 @@
 //camelCase-written;
 /*
 .1-Solve,untill 23 ex of programiz js section.
-.3-Read textbooks,until page.
-.4-Code with mosh loop & function videos.
-.5-For in & for of loop,self study on w3shools.
-.6-Rewrite,first season of web3 course in for moduel.
+.2-Read textbooks,until page.
+.3-Code with mosh loop & function videos.
+.4-For in & for of loop,self study on w3shools.
+.5-Rewrite,first season of web3 course in for moduel.
 */
+
+//1-programiz-s
+
+//13
+//end
+
+//14
+//end
+
+//15
+//end
+
+//16
+//end
+
+//17
+//end
+
+//18
+//end
+
+//19
+//end
+
+//20
+//end
+
+//21
+//end
+
+//22
+//end
+
+//23
+//end
+
+//1-programiz-e
+
+//2-textbooks-ex-s
+//2-textbooks-ex-e
+
+//3-code-wirh-mosh-s
+//3-code-wirh-mosh-e
+
+//4-forin-&-forof-ex-s
+//4-forin-&-forof-ex-e
+
+//5-rewirte-in-for-s
+//5-rewirte-in-for-e
 
 //written-by-illia.amini
 //mind-of-king-,-hear-of-warrior
