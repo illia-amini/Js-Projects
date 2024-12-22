1d99cefebbb6c9f3fb0b72b3a7f7216df02e98eb
illia-amini <illiaamini2002ir@gmail.com>
2024-12-22 15:41:43 +0330

    hm-03 started

--- Assect/Hm-03.js
+++ Assect/Hm-03.js
@@ -13,7 +13,29 @@
 
 //1-programiz-s
 
-//13
+//13-prime-number
+const number = parseInt(prompt("Enter a positive number: "));
+      let isPrime = true;
+      if (number === 1) {// check if number is equal to 1
+        alert("1 is neither prime nor composite number.");
+      }
+   
+      else if (number > 1) {   // check if number is greater than 1
+        for (let i = 2; i <= number / 2; i++) {// looping through 2 to number/2
+          if (number % i == 0) {
+            isPrime = false;
+            break;
+          }
+        }
+        if (isPrime) {
+          alert(`${number} is a prime number`);
+        } else {
+          alert(`${number} is a not prime number`);
+        }
+      }
+      else {
+        alert("The number is not a prime number.");// check if number is less than 1
+      }
 //end
 
 //14
