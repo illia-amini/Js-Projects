093277f71a044a2561c449b8819eea0e2fde0211
illia-amini <illiaamini2002ir@gmail.com>
2024-12-23 23:35:38 +0330

    Update Hm-03.js

--- Assect/Hm-03.js
+++ Assect/Hm-03.js
@@ -46,10 +46,10 @@ const number = parseInt(prompt("Enter a positive number: "));
 var factorialNum=parseInt(prompt('Enter a positive number: '));
 var factorial=1;
 if (factorialNum === 1){
-  alert('The Factorial of 1 is Zero.\n1!=0.')
+  alert('The Factorial of 1 is Zero.\n1!=0.');
 }else if(factorialNum > 1){
   for(let i=1;i<=factorialNum;i--){
-    var newFactorial =factorialNum*i
+    var newFactorial =factorialNum*i;
   }
   alert('Invalid Num');
 }
