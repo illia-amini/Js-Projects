f9f651516e8755ae2273d5cc27c5ca2d11aa941b
illia-amini <illiaamini2002ir@gmail.com>
2024-12-30 16:25:10 +0330

    Update Hm-04.js

--- Assect/Hm-04.js
+++ Assect/Hm-04.js
@@ -4,8 +4,47 @@
 //camelCase-written;
 
 /*
-.1-Solve,first 12 ex of programiz js section.(required)
+.1-write max() & min() & sum() codes by using arguments.(optional))
 .2-5 ex of if, 5 ex of switch.(required)
 .3-Read textbooks,until page 29.(optional)
 .4-Code with mosh opeartion videos.(optional)
 */
+
+//1-max() & min () & sum ()-s
+
+//1.min()
+function $cMin() {
+    console.log(arguments);
+    let result = arguments[0];
+    for (let nums of arguments) {
+      if (nums < result) {
+        result = nums;
+      }
+    }
+    console.log(result);
+  }
+  $cMin(10, 4, 45, 3, 1);// here u can what nums u want.
+//2.max()
+function $cMax() {
+    console.log(arguments);
+    let result = arguments[0];
+    for (let nums of arguments) {
+      if (nums > result) {
+        result = nums;
+      }
+    }
+    console.log(result);
+  }
+  $cMax(10, 22, 3, 4, 5, 55454, 3);// here u can what nums u want.
+  //3.sum()
+  function $cSum(){
+    console.log(arguments);
+    let sum=0;
+    for(let nums of arguments){
+        sum+=nums;
+    }
+    console.log(sum);
+  }
+$cSum(10,2,3);// here u can what nums u want.
+
+//1-max() & min () & sum ()-e
