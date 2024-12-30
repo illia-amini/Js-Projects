a91fa6a2bcf70986678d177e709efb380d7bdaf8
illia-amini <illiaamini2002ir@gmail.com>
2024-12-30 15:07:34 +0330

    a new projects added

---
+++ Assect/Javascript-pta.js
@@ -0,0 +1,37 @@
+ /*
+      pattern;
+      console log javascript as pattern below:
+      j
+      ja
+      jav
+      java
+      javas
+      javasc
+      javascr
+      javascri
+      javascrip
+      javascript
+      javascript
+      javascrip
+      javascri
+      javascr
+      javasc
+      javas
+      java
+      jav
+      ja
+      j
+      */
+      //upper pattern's code
+      var $java = "javascript";
+      var $count = [];
+      for (let i of $java) {
+        $count += i;
+        console.log($count);
+      }
+      //lower pattern's code
+      // ?? is how to star from end char to first char ?? 
+      // for (let i of $java) {
+      //   $count -= i;
+      //   console.log($count);
+      // }
