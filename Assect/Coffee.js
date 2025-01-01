3b43b4da09ff6dca9eee55e758238996511c4732
illia-amini <illiaamini2002ir@gmail.com>
2025-01-01 14:36:14 +0330

    Create Coffee.js

---
+++ Assect/Coffee.js
@@ -0,0 +1,7 @@
+let $userName=prompt('Enter ur name;');
+confirm(`Hello,${$userName},\nWelcome to ur pannel!`);
+let $coffee=parseInt(prompt('Tell us,ur Coffee Situtation First:'));
+alert('1-Empty\n2-Half-full\n3-Full');
+function $coffee(){
+    
+}
