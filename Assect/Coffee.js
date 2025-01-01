f6f50612f37e62c427a5241d2adf9a49f0734585
illia-amini <illiaamini2002ir@gmail.com>
2025-01-01 14:52:26 +0330

    Update Coffee.js

--- Assect/Coffee.js
+++ Assect/Coffee.js
@@ -1,7 +1,17 @@
-let $userName=prompt('Enter ur name;');
-confirm(`Hello,${$userName},\nWelcome to ur pannel!`);
-let $coffee=parseInt(prompt('Tell us,ur Coffee Situtation First:'));
-alert('1-Empty\n2-Half-full\n3-Full');
+
 function $coffee(){
-    
+    let $userName=prompt('Enter ur name;');
+    confirm(`Hello,${$userName},\nWelcome to ur pannel!`);
+    alert('Tell us,ur Coffee Situtation First:\n1-Empty\n2-Half-full\n3-Full');          
+    let $userCoffeeNum=Number(prompt('here;'));
+    if ($userCoffeeNum == 1){
+        return('No No!!!,U shouldnt start now\nFirst get ur coffee,\nRobusta preferably ;)');
+    }else if($userCoffeeNum == 2){
+        return('Hmmmmm!!!;( Not Bad,\nBut better to take a coffee first\nArabica perferably ;)');
+    }else if($userCoffeeNum == 3){
+        return(`Awesome,U can start ${$userName},\nGood Luck with Coding;)`);
+    }else{
+        return('Invalid Number!!!,Try again');
+    }
 }
+$coffee()
