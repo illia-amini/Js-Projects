21c44470c7bb18fa23bdcae2b75a21cd9a6d6416
illia-amini <illiaamini2002ir@gmail.com>
2025-01-14 17:43:03 +0330

    hm upadated

--- Js-Hm/Hm-05.js
+++ Js-Hm/Hm-05.js
@@ -6,4 +6,5 @@
 /*
 .1-write a dynamcic list to calc max & min age in server data.(required)
 .2-Crud(create,read,update,delete) sorce in js refrences.(optional)
+.3-5 ex of The function(Decloration,Experssion,Object(math),self-invoking).(required)
 */
