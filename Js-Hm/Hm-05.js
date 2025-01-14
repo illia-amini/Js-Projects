5c38ad82f0b002ae01bd8486580045647892b7cc
illia-amini <illiaamini2002ir@gmail.com>
2025-01-14 17:46:40 +0330

    Update Hm-05.js

--- Js-Hm/Hm-05.js
+++ Js-Hm/Hm-05.js
@@ -4,7 +4,7 @@
 //camelCase-written;
 
 /*
-.1-write a dynamcic list to calc max & min age in server data.(required)
+.1-write a dynamic list to calc max & min age in server data.(required)
 .2-Crud(create,read,update,delete) sorce in js refrences.(optional)
 .3-5 ex of The function(Decloration,Experssion,Object(math),self-invoking).(required)
 */
