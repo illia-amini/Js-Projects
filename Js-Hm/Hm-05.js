4b58f16b9eeca91f22962daaa063b63ed3b67aad
illia-amini <illiaamini2002ir@gmail.com>
2025-01-16 06:20:00 +0330

    Update Hm-05.js

--- Js-Hm/Hm-05.js
+++ Js-Hm/Hm-05.js
@@ -29,6 +29,57 @@
 
 //4-Read-all-func-s
 //1-Math-Func
+//1-abs()
+console.log(Math.abs(-5));//return absoloute value of -5 = 5.
+//2-ceil()*imp
+console.log(Math.ceil(5.1));//return 6, rounde to up.
+//3-floor()*imp
+console.log(Math.floor(5.9));//return 5, rounde to down.
+//4-round()*imp
+console.log(Math.round(5.5));//return 6, rounde to nearest int.
+//5-pow()
+console.log(Math.pow(2,3));//return 8, 2*2*2=8.For calculate power.
+//6-sqrt()
+console.log(Math.sqrt(9));//return 3, 3*3=9.For calculate square root.
+//7-random()*imp
+console.log(Math.random());//return random number between 0-1.
+//8-max()*imp
+console.log(Math.max(1,2,3,4,5));//return 5, return max number.
+//9-min()*imp
+console.log(Math.min(1,2,3,4,5));//return 1, return min number.
+//10-PI
+console.log(Math.PI);//return 3.141592653589793, return PI number.
+//11-E
+console.log(Math.E);//return 2.718281828459045, return E number.
+//12-log()
+console.log(Math.log(10));//return 2.302585092994046, return log number.
+//13-log10()
+console.log(Math.log10(10));//return 1, return log10 number.
+//14-log2()
+console.log(Math.log2(10));//return 3.321928094887362, return log2 number.
+//15-exp()
+console.log(Math.exp(1));//return 2.718281828459045, return E^number.
+//16-sin()
+console.log(Math.sin(1));//return 0.8414709848078965, return sin number.
+//17-cos()
+console.log(Math.cos(1));//return 0.5403023058681398, return cos number.
+//18-tan()
+console.log(Math.tan(1));//return 1.5574077246549023, return tan number.
+//19-atan()
+console.log(Math.atan(1));//return 0.7853981633974483, return atan number.
+//20-atan2()
+console.log(Math.atan2(1,1));//return 0.7853981633974483, return atan2 number.
+//21-found()
+//returns the nearest 32-bit single precision float representation of a number.
+//22-sign()*imp
+console.log(Math.sign(-5));//return -1, return sign number.
+//23-trunc()*imp
+console.log(Math.trunc(5.5));//return 5, return int part of decimal number.
+//24-cbrt()
+console.log(Math.cbrt(8));//return 2, return cubic root of number.
+//25-hypot()
+console.log(Math.hypot(3,4));//return 5, return hypotenuse of right triangle.
+
 //2-Date-Func
 //3-Num-Func
 //4-Str-Func
