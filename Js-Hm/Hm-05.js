1eb87e3dd16af5cec8215a15595b95c9f8308ba9
illia-amini <illiaamini2002ir@gmail.com>
2025-01-16 05:47:26 +0330

    some changes appliead

--- Js-Hm/Hm-05.js
+++ Js-Hm/Hm-05.js
@@ -7,4 +7,8 @@
 .1-write a dynamic list to calc max & min age in server data.(required)
 .2-Crud(create,read,update,delete) sorce in js refrences.(optional)
 .3-5 ex of The function(Decloration,Experssion,Object(math),self-invoking).(required)
+.4-Read all func in Array,Obj,Math,Str,Num,Date.(required)  
 */
+
+//Written-by-illia.amini
+//Mind-of-king-,-Heart-of-warrior   
