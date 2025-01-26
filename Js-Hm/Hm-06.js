33b7157546a9e4d1601acbac83b1e6920733cc3e
illia-amini <illiaamini2002ir@gmail.com>
2025-01-26 16:24:20 +0330

    Update Hm-06.js

--- Js-Hm/Hm-06.js
+++ Js-Hm/Hm-06.js
@@ -7,6 +7,7 @@
 .1-10 ex of class in js methode & proprities (required)
 .2-Setter & getter.(optional)
 .3-Create 3 buttons,with dynamic pic.(required)
+.4-timer & chornometer.(required)
 */
 
 //Written-by-illia.amini
