1fcab72c847525397c81b5d39312c93c3ba6cac3
illia-amini <illiaamini2002ir@gmail.com>
2025-01-26 17:24:24 +0330

    Update Hm-06.js

--- Js-Hm/Hm-06.js
+++ Js-Hm/Hm-06.js
@@ -8,7 +8,11 @@
 .2-Setter & getter.(optional)
 .3-Create 3 buttons,with dynamic pic.(required)
 .4-timer & chornometer.(required)
+.5-solve js examples untill 50.(required)
+.6-Aos.(Animation on scroll)(required)
+.7-swiffer slider.(required)
 */
 
+//5 & 6 examples  are in written in thier own files.
 //Written-by-illia.amini
 //Mind-of-king-,-Heart-of-warrior
