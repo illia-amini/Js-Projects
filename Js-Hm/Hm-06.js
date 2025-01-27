2da74720e9a3354f76d13eb26a12e8efba53fc55
illia-amini <illiaamini2002ir@gmail.com>
2025-01-27 19:22:45 +0330

    Update Hm-06.js

--- Js-Hm/Hm-06.js
+++ Js-Hm/Hm-06.js
@@ -66,9 +66,93 @@ why do we perfer to use getter & setter?
 2-it can secuer better data quality.
 3-it allows equal syntax for propreties and methode.
 4-it is useful for doing things behind-the-scenes.
+
+Object.defineProperty()***IDK
+
+All ex of setter & getter
+
+ //Setter
+      const person = {
+        firstName: "Ali",
+        lastName: "Amini",
+        age: 22,
+        languge: "",
+        set lang(lang) {
+          this.languge = lang;
+        },
+      };
+      person.lang = "Fa";
+      console.log(person.languge);
+      //Getter
+      const person2 = {
+        firstName: "Ali",
+        lastName: "Amini",
+        age: 22,
+        languge: "Fa",
+        get lang() {
+          return this.languge;
+        },
+      };
+      console.log(person2.lang);
+      //Function & Proprety
+      //1-Function
+      const person3 = {
+        firstName: "Ali",
+        lastName: "Amini",
+        age: 22,
+        languge: "En",
+        fullName: function () {
+          return this.firstName + " " + this.lastName;
+        },
+      };
+      console.log(person3.fullName); //it returns a function and u cant visit the proprities.
+      //2-Proprety
+      const person4 = {
+        firstName: "Ali",
+        lastName: "Amini",
+        age: 22,
+        languge: "En",
+        get fullName() {
+          return this.firstName + " " + this.lastName;
+        },
+      };
+      console.log(person4.fullName);
+      //Data Quality - Getter
+      const person5 = {
+        firstName: "Ali",
+        lastName: "Amini",
+        age: 22,
+        languge: "fa",
+        get lang() {
+          return this.languge.toUpperCase();
+        },
+      };
+      console.log(person5.lang);
+      console.log(person5); //!!!! why we see lang proprety in object as lang!!!
+      //Datat Quality - Setter
+      const person6 = {
+        firstName: "Ali",
+        lastName: "Amini",
+        age: 22,
+        languge: "fa",
+        set lang(lang) {
+          this.languge = lang.toUpperCase();
+        },
+      };
+      person6.lang = "fa";
+      console.log(person6.languge);
 */
 
 //2-Setter & Getter-e
-//5 & 6 examples  are in written in thier own files.
+
+//3-3-btn-s
+//3-3-btn-e
+//4- examples  are in written in thier own files.
+//5-10-ex-s
+//5-10-ex-e
+//6-Aos-s
+//6-Aos-e
+//7-swiffer-slider-s
+//7-swiffer-slider-e
 //Written-by-illia.amini
 //Mind-of-king-,-Heart-of-warrior 
