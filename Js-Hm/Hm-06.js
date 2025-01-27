ba0bcb6c8af1cce03b654f2b3040c4562fe33c76
illia-amini <illiaamini2002ir@gmail.com>
2025-01-27 18:20:40 +0330

    Update Hm-06.js

--- Js-Hm/Hm-06.js
+++ Js-Hm/Hm-06.js
@@ -15,6 +15,58 @@
 //1-Class-ex-s
 //1-Class-ex-e
 //2-Setter & Getter-s
+/*
+Getters & Setters allow u to define OA(Object Accessories)(Computed Properties)
+1-Getter (The get keyword):
+.lang proprety:to get the value of the languge proprety
+ex;
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "en",
+  get lang() {
+    return this.language;
+  }
+};
+
+// Display data from the object using a getter:
+document.getElementById("demo").innerHTML = person.lang;
+ex;
+const person = {
+        firstName: "Ali",
+        lastName: "Amini",
+        age: 22,
+        languge: "Fa",
+        get lang() {
+          return this.languge;
+        },
+      };
+      console.log(person.lang);
+
+2-Setter(To Set keyword)
+.lang proprety; to set the value of the languge proprety.
+ex;
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "",
+  set lang(lang) {
+    this.language = lang;
+  }
+};
+
+// Set an object property using a setter:
+person.lang = "en";
+
+// Display data from the object:
+document.getElementById("demo").innerHTML = person.language;
+
+why do we perfer to use getter & setter?
+1-gives simpler syntax
+2-it can secuer better data quality.
+3-it allows equal syntax for propreties and methode.
+4-it is useful for doing things behind-the-scenes.
+*/
 
 //2-Setter & Getter-e
 //5 & 6 examples  are in written in thier own files.
