62ddc35d893d84a9be62cf5feff950a83ce08d72
illia-amini <illiaamini2002ir@gmail.com>
2025-02-04 05:10:05 +0330

    Update Hm-07.js

--- Js-Hm/Hm-07.js
+++ Js-Hm/Hm-07.js
@@ -7,5 +7,12 @@
 1-Dropdown by mouseover & mouseout (reqired)
 */
 
+
+//1-Dropdown-s
+/*
+written in,
+Dropdown project.
+*/
+//1-Dropdown-e
 //Written-by-illia.amini
 //Mind-of-king-,-Heart-of-warrior
