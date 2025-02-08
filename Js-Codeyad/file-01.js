ddc153c09ddc119b8210778dcab2f59ca51b0460
illia-amini <illiaamini2002ir@gmail.com>
2025-02-08 15:20:55 +0330

    Update file-01.js

--- Js-Codeyad/file-01.js
+++ Js-Codeyad/file-01.js
@@ -17,7 +17,7 @@ js-comment(/**).
 it will not read by js
 */
 /* 
-js-datatype(int,str,float).
+js-datatype(int,str,float,obj).
 all types of varaibles in js.
 in js varaibles define from left to right if its str it behaves with others like str and vice versa,
 */
@@ -27,3 +27,8 @@ var c01=23 + 'ali';/* it will print 23 ali, js will behave like str with 23 and 
 var c02=45-23+'ALI'/* first it will apply math compration,then put the result next to the str */
 var c02= 'ALI' + 45-23/*it will print NAn,first it reads from left to right,  */
 var c02= 'ALI' + 45+23/*it will print Ali4523, js can not define yhat 45+23 is either a math comp otr str, then in order to rule sit behaves like a str to it  */
+/*
+what object means?
+obj is,anything which has a some features it is called obj
+for ex; a car, a phone and..., they have many of features which included. 
+*/
