1788f81f9207a8ed4a6976ceb173ced4beffd199
illia-amini <illiaamini2002ir@gmail.com>
2025-02-08 15:02:43 +0330

    Update file-01.js

--- Js-Codeyad/file-01.js
+++ Js-Codeyad/file-01.js
@@ -1,13 +1,18 @@
 /*
 js-syntax (;).
+js defeintion.
 */
 console.log('Connected...');
 /*
 js-Varaibles (var).
+define a varaible in js
 */
 var fName='Ali';var lName='Amini'
 console.log(fName);
 console.log(lName);
+document.write(fName,lName)/* it will write  on html page */
+window.alert(fName);
 /* 
-js-
+js-comment(/**).
+it will not read by js
 */
