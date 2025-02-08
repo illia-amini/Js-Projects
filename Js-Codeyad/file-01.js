73bc0b4ad081da69b3f98814ec1e92edcd1fc0bb
illia-amini <illiaamini2002ir@gmail.com>
2025-02-08 14:46:33 +0330

    Create file-01.js

---
+++ Js-Codeyad/file-01.js
@@ -0,0 +1,13 @@
+/*
+js-syntax (;).
+*/
+console.log('Connected...');
+/*
+js-Varaibles (var,let,const).
+*/
+var fName='Ali';
+let lName='Amini';
+const age = 22;
+console.log(fName);
+console.log(lName);
+console.log(age);
