9355f210f717ec6d61c207dcb4aecea27942d8aa
Alireza Amini <illiaamini2002ir@gmail.com>
2025-03-20 06:05:51 +0330

    Update file-01.js

--- Js-Codeyad-Tutorial/file-01.js
+++ Js-Codeyad-Tutorial/file-01.js
@@ -1002,8 +1002,8 @@ function fillInput(element) {
 //Its a techonnology to make cconnection betwwen client & server easier.
 //To be able to use ajax. u should use of an object name is XMLHttpRequest()
 //Explantion of code below:
-//We must write new XmlHttpRequest() in a new varaible ,and create a new of this opject, then we must use open funcc to acees to inside of it,insside of the openn func we write 1-requeste type 2-destiontion url 3-
-//we diffen for our var in unload a funnc to check its status here if its succesfully will retutn this response.
+//We must write new XmlHttpRequest() in a new varaible ,and create a new of this opject, then we must use open funcc to acees to inside of it,insside of the openn func we write 1-requeste type 2-destiontion url 3-Syncronose(true) or inSyncronose(false)
+//we diffen for our var in unload a funnc to check its status here if its succesfully will retutn this responses.
 //then we ssennd our request by using send func
 //ex;
     {/* <button id="my-btn">Click on me!</button>
@@ -1025,3 +1025,9 @@ function fillInput(element) {
     //     xhr.send();
     // }
     // mybtn.addEventListener('click',loadTxt)
+
+
+    //syncronose & insyncronose; the methode of applying or running of our code,
+    //Syncronse; means line by line and at the same time.
+    //insyncronose; means not at the same time not line by line all our codes will apply at once  and its not related to its previoud or fllowind codes,
+    //  .
