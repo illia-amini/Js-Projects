d1f63f273f0bc7cce26c19500aa4581c3f9ae739
illia-amini <illiaamini2002ir@gmail.com>
2025-03-23 21:03:49 +0330

    Update Script-Ajax.js

--- Js-Codeyad-Tutorial/Ajax/Script-Ajax.js
+++ Js-Codeyad-Tutorial/Ajax/Script-Ajax.js
@@ -3,13 +3,24 @@ const txtDiv = document.getElementById("text");
 function loadText() {
   let xhr = new XMLHttpRequest();
   //   console.log(xhi);
+
   xhr.open("GET", "Ajax-tutorial.txt", true);
+  console.log("Redystate:", xhr.readyState); //redystate is 1
+  xhr.onprogress = function () {
+    console.log("Redystate:", xhr.readyState); //readystate is 3
+  };
   xhr.onload = function () {
     if (xhr.status == 200) {
       txtDiv.innerHTML = this.response;
     }
   };
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      txtDiv.innerHTML = this.responseText;
+    }
+  };
   xhr.send();
+  console.log("Redystate:", xhr.readyState); //readystate is
 }
 
 myBtn.addEventListener("click", loadText);
