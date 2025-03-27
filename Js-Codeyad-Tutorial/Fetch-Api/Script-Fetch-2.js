58b73ff51eb9d9bb21ebf2136d41b34aa92f89cd
illia-amini <illiaamini2002ir@gmail.com>
2025-03-27 09:53:15 +0330

    project-02 of fetch api

--- Js-Codeyad-Tutorial/Fetch-Api/Script-Fetch-2.js
+++ Js-Codeyad-Tutorial/Fetch-Api/Script-Fetch-2.js
@@ -1,7 +1,19 @@
 function getUsers() {
-  fetch("").then((res) => {
-    console.log(res.text()).then((data) => {
-      console.log(data);
+  fetch("users-2.json").then((res) => {
+    res.json().then((data) => {
+      // console.log(data);//here u can see the json, via using json func()
+      let myDiv = " ";
+      forEach(function (user) {
+        myDiv += `<ul>
+        <li>${user.id}</li>
+        <li>${user.name}</li>
+        <li>${user.email}</li>
+      </ul>
+            `;
+        //Erorr, in promises for each is not a function is returend ???
+      });
+      document.getElementById("outpit").innerHTML = myDiv;
     });
   });
 }
+btnGetUsers.addEventListener("click", getUsers);
