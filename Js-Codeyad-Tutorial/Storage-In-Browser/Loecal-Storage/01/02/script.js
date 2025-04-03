949546c6ae756c35375492e850657113336d29d8
illia-amini <illiaamini2002ir@gmail.com>
2025-04-03 14:58:52 +0330

    local storage 2

---
+++ Js-Codeyad-Tutorial/Storage-In-Browser/Loecal-Storage/01/02/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+// localStorage.setItem("FirstName", "reza");
+// localStorage.setItem("LastName", "Amini");
+button.addEventListener("click").localStorage.setitem("FirstName", "reza");
+button.addEventListener("click").localStorage.setitem("lastName", "amini");
