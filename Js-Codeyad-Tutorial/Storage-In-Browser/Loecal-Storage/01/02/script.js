73d3ae56284173ef82f4b39fd01c54bda48b457e
illia-amini <illiaamini2002ir@gmail.com>
2025-04-03 15:02:00 +0330

    Update script.js

--- Js-Codeyad-Tutorial/Storage-In-Browser/Loecal-Storage/01/02/script.js
+++ Js-Codeyad-Tutorial/Storage-In-Browser/Loecal-Storage/01/02/script.js
@@ -1,6 +1,7 @@
-const button = document.getElementById("button");
-const input = document.getElementById("input");
+const button = document.getElementById("test");
+const input = document.getElementById("text");
 // localStorage.setItem("FirstName", "reza");
 // localStorage.setItem("LastName", "Amini");
+
 button.addEventListener("click").localStorage.setitem("FirstName", "reza");
 button.addEventListener("click").localStorage.setitem("lastName", "amini");
